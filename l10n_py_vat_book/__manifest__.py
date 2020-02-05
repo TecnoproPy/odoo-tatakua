11387b7b7a2e05f77d70400bc03a8f1fc8cab626
jobiols <jorge.obiols@gmail.com>
2020-02-04 22:48:02 -0300

    [FIX] missing files

---
+++ l10n_py_vat_book/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Libros de IVA Paraguay',
+    'version': '13.0.0.0.4',
+    'category': 'Localizacion',
+    'sequence': 14,
+    'author': 'Tecnopro',
+    'website': 'tecnopro.com.py',
+    'license': 'Other OSI approved licence',
+    'summary': 'Libros de IVA',
+    "development_status": "Alpha",  # "Alpha|Beta|Production/Stable|Mature"
+    'depends': [
+        'l10n_py',
+        'l10n_py_reports',
+        'l10n_py_invoice_document',
+        'account_reports',
+    ],
+    'data': [
+        'report/account_py_vat_line_views.xml',
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'data/account_financial_report_data.xml'
+    ],
+    'installable': True,
+}
