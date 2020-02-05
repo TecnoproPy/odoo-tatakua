5c17ff3408d62af32f127d446a54638ffc6c1468
jobiols <jorge.obiols@gmail.com>
2020-02-05 01:57:35 -0300

    Faltaba una coma

--- l10n_py/__manifest__.py
+++ l10n_py/__manifest__.py
@@ -11,7 +11,7 @@
     'summary': 'Modulo base para la localizacion Paraguaya',
     'depends': [
         'account',
-        'l10n_latam_invoice_document'
+        'l10n_latam_invoice_document',
         'l10n_generic_coa',
     ],
     'data': [
